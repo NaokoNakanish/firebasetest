e3b1b00d84af8f1d1af854ab6c4cecee83ebdaec
naokonakanish <fu2fuka.nao@gmail.com>
2021-11-05 16:59:15 +0900

    admin info add

--- app/app.js
+++ app/app.js
@@ -2,9 +2,16 @@ import Vue from "nativescript-vue";
 
 import Home from "./components/Home";
 
-// NativeScript 7+
+// Nativescript/firebase setting ver NativeScript 7+
 var firebase = require("@nativescript/firebase").firebase;
 
+// set up for Cloud Firestore https://firebase.google.com/docs/firestore/quickstart#initialize
+// これやると、'Package subpath './package.json' is not defined by "exports" in /Users/nn/firebasetest/node_modules/firebase-admin/package.json'と出て、シミュレータ動かなくなるのなんでだ　https://github.com/xerroxcopy/news-native/issues/208#issuecomment-961689726
+// const admin = require('firebase-admin');
+// admin.initializeApp();
+// const db = admin.firestore();
+
+// Nativescript/firebase setting
 firebase
   .init({
     // Optionally pass in properties for database, authentication and cloud messaging,
