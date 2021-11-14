7d80cbce7582febb89ce45b0bae69bc26bf66cd5
naokonakanish <fu2fuka.nao@gmail.com>
2021-11-14 11:26:10 +0900

    change for checking Tab component / this does not work

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -4,14 +4,40 @@
       <Label text="Home" />
     </ActionBar>
 
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; " />
-          <Span :text="message" />
-        </FormattedString>
-      </Label>
-    </GridLayout>
+    <StackLayout row="1">
+      <Tabs>
+        <TabStrip>
+          <TabStripItem>
+            <Label text="Newest" />
+          </TabStripItem>
+          <TabStripItem>
+            <Label text="Daily" />
+          </TabStripItem>
+          <TabStripItem>
+            <Label text="Weekly" />
+          </TabStripItem>
+          <TabStripItem>
+            <Label text="Monthly" />
+          </TabStripItem>
+        </TabStrip>
+
+        <TabContentItem>
+          <Label text="NEWEST test" />
+        </TabContentItem>
+
+        <TabContentItem>
+          <Label text="DAILY test" />
+        </TabContentItem>
+
+        <TabContentItem>
+          <Label text="WEEKLY test" />
+        </TabContentItem>
+
+        <TabContentItem>
+          <Label text="MONTHLY test" />
+        </TabContentItem>
+      </Tabs>
+    </StackLayout>
   </Page>
 </template>
 
@@ -19,25 +45,25 @@
 import { firebase } from "@nativescript/firebase";
 
 export default {
-  computed: {
-    async message() {
-      const Ref = firebase.firestore
-        .collection("comments")
-        .doc("07bhQeWDf3u1j0B4vNwG");
-      const doc = await Ref.get();
-      const hoge = doc.data();
-      console.log("hoge.commented_at", hoge.commented_at); // CONSOLE LOG: hoge.commented_at Sat Oct 23 2021 22:44:48 GMT+0900 (JST)
-      console.log(
-        "hoge.commented_at in milliseconds: ",
-        Math.floor(hoge.commented_at / 1000)
-      ); // CONSOLE LOG: hoge.commented_at in milliseconds:  1634996688
-      const getNewDate = new Date("October 23, 2021, 22:44:48 GMT+0900"); // same as hoge.commented_at
-      console.log("getNewDate in milliseconds: ", getNewDate.getTime() / 1000); // CONSOLE LOG: getNewDate in milliseconds:  1634996688
-      const hogeToDate = hoge.toDate();
-      console.log("hogeToDate", hogeToDate);
-      return hogeToDate;
-    },
-  },
+  //   computed: {
+  //     async message() {
+  //       const Ref = firebase.firestore
+  //         .collection("comments")
+  //         .doc("07bhQeWDf3u1j0B4vNwG");
+  //       const doc = await Ref.get();
+  //       const hoge = doc.data();
+  //       console.log("hoge.commented_at", hoge.commented_at); // CONSOLE LOG: hoge.commented_at Sat Oct 23 2021 22:44:48 GMT+0900 (JST)
+  //       console.log(
+  //         "hoge.commented_at in milliseconds: ",
+  //         Math.floor(hoge.commented_at / 1000)
+  //       ); // CONSOLE LOG: hoge.commented_at in milliseconds:  1634996688
+  //       const getNewDate = new Date("October 23, 2021, 22:44:48 GMT+0900"); // same as hoge.commented_at
+  //       console.log("getNewDate in milliseconds: ", getNewDate.getTime() / 1000); //
+  //       const hogeToDate = hoge.toDate();
+  //       console.log("hogeToDate", hogeToDate);
+  //       return hogeToDate;
+  //     },
+  //   },
 };
 </script>
 
